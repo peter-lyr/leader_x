13eae2a18b4e624980d119f59e9a94e113c8a100
peter-lyr <llydrp.ldp@gmail.com>
2024-09-16 12:08:56 +0800

    require 'which-key'.register

--- plugin/leader_x.lua
+++ plugin/leader_x.lua
@@ -1,9 +1,9 @@
 local F = require 'f'
 
-require 'which-key'.add {
-  { '<leader>x',   group = 'window.delete', },
-  { '<leader>xj', function() F.window_delete 'j' end,  desc = 'window delete down',  mode = { 'n', 'v', }, },
-  { '<leader>xk', function() F.window_delete 'k' end,  desc = 'window delete up',    mode = { 'n', 'v', }, },
-  { '<leader>xh', function() F.window_delete 'h' end,  desc = 'window delete left',  mode = { 'n', 'v', }, },
-  { '<leader>xl', function() F.window_delete 'l' end,  desc = 'window delete right', mode = { 'n', 'v', }, },
+require 'which-key'.register {
+  ['<leader>x'] = { name = 'window.delete', },
+  ['<leader>xj'] = { function() F.window_delete 'j' end, 'window delete down', mode = { 'n', 'v', }, },
+  ['<leader>xk'] = { function() F.window_delete 'k' end, 'window delete up', mode = { 'n', 'v', }, },
+  ['<leader>xh'] = { function() F.window_delete 'h' end, 'window delete left', mode = { 'n', 'v', }, },
+  ['<leader>xl'] = { function() F.window_delete 'l' end, 'window delete right', mode = { 'n', 'v', }, },
 }
