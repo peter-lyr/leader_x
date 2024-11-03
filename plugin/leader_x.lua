6b1f782febe806a658aa35f3d7c28f2b49b2d0f8
peter-lyr <llydrp.ldp@gmail.com>
2024-11-03 12:31:07 +0800

    <leader>x<leader>x F.bin_xxd_sel()

--- plugin/leader_x.lua
+++ plugin/leader_x.lua
@@ -6,4 +6,7 @@ require 'which-key'.register {
   ['<leader>xk'] = { function() F.window_delete 'k' end, 'window delete up', mode = { 'n', 'v', }, },
   ['<leader>xh'] = { function() F.window_delete 'h' end, 'window delete left', mode = { 'n', 'v', }, },
   ['<leader>xl'] = { function() F.window_delete 'l' end, 'window delete right', mode = { 'n', 'v', }, },
+
+  ['<leader>xx'] = { function() F.bin_xxd() end, 'xxd', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>x<leader>x'] = { function() F.bin_xxd_sel() end, 'xxd', mode = { 'n', 'v', }, silent = true, },
 }
