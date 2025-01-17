662bae1c9e78b5fab3c807a232129af2244bf110
peter-lyr <llydrp.ldp@gmail.com>
2025-01-17 23:41:16 +0800

    <leader>xt F.tabclose()

--- plugin/leader_x.lua
+++ plugin/leader_x.lua
@@ -6,7 +6,7 @@ require 'which-key'.register {
   ['<leader>xk'] = { function() F.window_delete 'k' end, 'window delete up', mode = { 'n', 'v', }, },
   ['<leader>xh'] = { function() F.window_delete 'h' end, 'window delete left', mode = { 'n', 'v', }, },
   ['<leader>xl'] = { function() F.window_delete 'l' end, 'window delete right', mode = { 'n', 'v', }, },
-  ['<leader>xt'] = { function() vim.cmd 'tabclose' end, 'tabclose', mode = { 'n', 'v', }, },
+  ['<leader>xt'] = { function() F.tabclose() end, 'tabclose', mode = { 'n', 'v', }, },
 
   ['<leader>xx'] = { function() F.bin_xxd() end, 'xxd', mode = { 'n', 'v', }, silent = true, },
   ['<leader>x<leader>x'] = { function() F.bin_xxd_sel() end, 'xxd', mode = { 'n', 'v', }, silent = true, },
